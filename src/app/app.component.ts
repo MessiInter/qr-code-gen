129d8cf311cd7746d7ca3e24db909bb58a29128f
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2023-12-01 17:25:39 +0000

    Update 9 files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'qr-code-gen-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'qr-code-gen';
-}
+export class AppComponent {}
