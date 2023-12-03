36d237906446bccc8de9399b4955dd13a0cbc43b
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2023-12-03 10:34:03 +0000

    Delete 1 file

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LANG=C.UTF-8 git -c color.status=false status \
-  | sed -n -r -e '1,/Changes to be commited:/ d' \
-  -e '1,1 d' \
-  -e '/^Untracked files:/,$ d' \
-  -e 's/^\s*//' \
-  -e '/./p' \
-  | git commit -m - \
-  && git push
