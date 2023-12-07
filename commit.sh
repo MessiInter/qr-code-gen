3bf04e2c752af979eeb56af03cb05139c84c0199
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2023-12-07 12:50:14 +0000

    modified:   .vscode/settings.json modified:   commit.sh

--- commit.sh
+++ commit.sh
@@ -2,10 +2,21 @@
 
 LANG=C.UTF-8
 
+git add --all
+
 git -c color.status=false status \
   | sed -n -r -e '1,/Changes to be committed:/ d' \
+              -e '/Changes not staged for commit:/ d' \
+              -e '/(use "git add <file>..." to update what will be committed)/ d' \
+              -e '/(use "git restore <file>..." to discard changes in working directory)/ d' \
+              -e '/(use "git restore --staged <file>..." to unstage)/ d' \
               -e '1,1 d' \
               -e '/^Untracked files:/,$ d' \
               -e 's/^\s*//' \
               -e '/./p' \
+  | awk '!seen[$0]++' \
   | git commit -F -
+
+if [ $1 != '--no-push' || $1 != '-np' ]; then
+  git push
+fi
