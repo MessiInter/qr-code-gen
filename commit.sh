2948733dfed4bb041e89b8417a05f84738f444e4
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2023-12-07 11:25:53 +0000

    (use "git restore --staged <file>..." to unstage) modified:   .vscode/settings.json modified:   commit.sh

--- commit.sh
+++ commit.sh
@@ -1,8 +1,15 @@
-# LANG=C.UTF-8 or any UTF-8 English locale supported by your OS may be used
-LANG=C git -c color.status=false status \
-| sed -n -r -e '1,/Changes to be committed:/ d' \
-            -e '1,1 d' \
-                        -e '/^Untracked files:/,$ d' \
-                                    -e 's/^\s*//' \
-                                                -e '/./p' \
-                                                | git commit -F -
+#!/bin/bash
+
+LANG=C.UTF-8
+
+git add --all
+
+git -c color.status=false status \
+  | sed -n -r -e '1,/Changes to be committed:/ d' \
+              -e '1,1 d' \
+              -e '/^Untracked files:/,$ d' \
+              -e 's/^\s*//' \
+              -e '/./p' \
+  | git commit -F -
+
+git push
