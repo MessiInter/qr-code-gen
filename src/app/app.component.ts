c68bcc1dc9eb692c9ab637bdbd9d598687e7b4b3
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2023-12-19 12:05:17 +0000

    Update .devcontainer/devcontainer.json, src/app/app.component.html and 1 more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -19,4 +19,11 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  initialState: { qrCodeData: string } = {
+    qrCodeData: 'https://github.com/MessiInter/qr-code-gen',
+  };
+
+  qrDataForm: FormControl = new FormControl();
+  qrData: string = '';
+}
