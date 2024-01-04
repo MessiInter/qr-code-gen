437d085061ed2ddc8123adc7a43f43efac7ac805
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-01-04 14:58:30 +0000

    Add/update docs/3rdpartylicenses.txt, docs/favicon.ico and 27 more

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,6 @@
+import './polyfills.server.mjs';
+import { a } from './chunk-BBPKW3XJ.mjs';
+import './chunk-7YSW2TDQ.mjs';
+import './chunk-4S7LW3SP.mjs';
+export { a as default };
+//# sourceMappingURL=main.server.mjs.map
