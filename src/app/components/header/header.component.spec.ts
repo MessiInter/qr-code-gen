f133614e95a4e0ed17e12ed7b139b2e84fe71c6c
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-01-07 10:49:03 +0000

    Update src/app/app.component.spec.ts, src/app/components/header/header.component.spec.ts and 4 more

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -5,6 +5,7 @@ import { HeaderComponent } from './header.component';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,10 +14,22 @@ describe('HeaderComponent', () => {
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    compiled = fixture.nativeElement as HTMLElement;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create header component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    expect(
+      compiled.querySelector('header.app-header a h1')?.textContent
+    ).toContain('QR Code Generator');
+  });
+
+  it(`should have as title 'QR Code Generator'`, () => {
+    expect(component.title).toEqual('QR Code Generator');
   });
 });
