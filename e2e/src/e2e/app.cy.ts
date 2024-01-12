4f6a32f1f12a4b6d0369a7c2b560e6ac101f33c8
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-01-12 16:49:28 +0000

    Add/update e2e/.gitignore, e2e/src/e2e/app.cy.ts and 2 more

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,25 +1,22 @@
-import { Decoder } from '@nuintun/qrcode';
-
-const decoder: Decoder = new Decoder();
-
 describe('e2e', () => {
-  let imageSrc: string = '';
-
   beforeEach(() => cy.visit('/'));
 
-  it('should render', () => cy.get('qr-code-gen-root').should('exist'));
+  it('should render', () =>
+    cy
+      .get('qr-code-gen-root')
+      .should('be.visible')
+      .should('exist')
+      .should('be.visible'));
 
-  it('should takes qrcode screenshot', () =>
-    cy.get('.qrcode qrcode').screenshot('qrcode', {
-      onAfterScreenshot($el, { path }) {
-        imageSrc = path;
-      },
-    }));
+  it('should render qrcode', () => {
+    cy.get('.qrcode qrcode').should('exist').should('be.visible');
+  });
 
-  it(`qrcode data after decode should equal to 'https://github.com/MessiInter/qr-code-gen'`, () =>
-    decoder
-      .scan(imageSrc)
-      .then(({ data }) =>
-        expect(data).to.equal('https://github.com/MessiInter/qr-code-gen')
-      ));
+  it('download button should work (img/png)', () => {
+    cy.get('.download-btn button').should('be.visible').click();
+  });
+
+  it('show advanced options button should work', () => {
+    cy.get('mat-checkbox.options-checkbox').should('be.visible').click();
+  });
 });
