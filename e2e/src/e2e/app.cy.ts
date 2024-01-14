921587436ecbb34edf712046d3c69f75c581067f
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-01-14 17:29:01 +0000

    Update e2e/src/e2e/app.cy.ts, package.json and 2 more

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,22 +1,24 @@
+import { Decoder } from '@nuintun/qrcode';
+
+const { scan } = new Decoder();
+const download: string = Cypress.config('downloadsFolder');
+
 describe('e2e', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should render', () =>
-    cy
-      .get('qr-code-gen-root')
-      .should('be.visible')
-      .should('exist')
-      .should('be.visible'));
+  it('should render', () => {
+    cy.get('qr-code-gen-root').should('exist').should('be.visible');
+  });
 
   it('should render qrcode', () => {
     cy.get('.qrcode qrcode').should('exist').should('be.visible');
   });
 
   it('download button should work (img/png)', () => {
-    cy.get('.download-btn button').should('be.visible').click();
+    cy.get('.download-btn button').should('exist').should('be.visible').click();
   });
 
-  it('show advanced options button should work', () => {
-    cy.get('mat-checkbox.options-checkbox').should('be.visible').click();
-  });
+  it('download files should exist', () => {});
+
+  it('qrcode files after decode should be correct', () => {});
 });
