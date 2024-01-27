9115ac8f10ce4fac39ca363947a8a7c8c506adb6
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-01-27 04:59:18 +0000

    Add/update jest.config.app.ts, .eslintignore and 5 more

--- jest.config.ts
+++ jest.config.ts
@@ -1,29 +1,3 @@
-/* eslint-disable */
 export default {
-  displayName: 'qr-code-gen',
-  preset: './jest.preset.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: './coverage/qr-code-gen',
-  transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
-      'jest-preset-angular',
-      {
-        tsconfig: '<rootDir>/tsconfig.spec.json',
-        stringifyContentPathRegex: '\\.(html|svg)$',
-      },
-    ],
-  },
-  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$|nanoid)'],
-  moduleNameMapper: {
-    '^nanoid(/(.*)|$)': 'nanoid$1',
-  },
-  snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
-  ],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
-    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
-  ],
+  projects: ['<rootDir>/jest.config.app.ts'],
 };
