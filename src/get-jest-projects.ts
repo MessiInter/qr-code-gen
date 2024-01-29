5321e27f399cf822c95c3e6b7aadea5400ca83cb
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-01-29 16:55:53 +0000

    Update .eslintrc.app.json, .prettierrc.js and 38 more

--- src/get-jest-projects.ts
+++ src/get-jest-projects.ts
@@ -3,7 +3,7 @@
  * Copyright MessiInter and contributors All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
- * found in the LICENSE.md file at the root of this project.
+ * found in the LICENSE.md file at https://github.com/MessiInter/qr-code-gen/blob/master/LICENSE.md
  */
 
 import {join, relative, resolve} from 'node:path';
