f92044a60e643ff554cb7bb8e31acba4f69d258b
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-02-05 12:55:58 +0000

    Add/update bin/commit-msg-checker.js, commitlint.config.js and 3 more

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright MessiInter and contributors All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE.md file at https://github.com/MessiInter/qr-code-gen/blob/master/LICENSE.md
+ */
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      ['Add', 'Add/update', 'Remove', 'Update', 'Docs'],
+    ],
+    'scope-case': [2, 'always', 'kebab-case'],
+    'scope-empty': [2, 'always'],
+    'references-empty': [2, 'never'],
+    'body-leading-blank': [2, 'always'],
+    'body-max-length': [2, 'always', 100],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-length': [2, 'always', 100],
+    'scope-full-path': [2, 'always', null],
+  },
+};
