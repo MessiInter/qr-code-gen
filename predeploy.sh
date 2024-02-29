d644ecf96c331b8e7fcb643796981b6da15e05ed
LionelMessi <136736943+MessiInter@users.noreply.github.com>
2024-02-29 14:55:58 +0000

    Update docs/app/index.html and predeploy.sh

--- predeploy.sh
+++ predeploy.sh
@@ -2,10 +2,13 @@
 set -e
 
 REPO_NAME=$(node -p "require(\"git-repo-name\").sync()")
+BUILD_OUTPUT=./docs/app
 
-pnpm build -- --baseHref=\"/$REPO_NAME/\" --outputPath=\"./docs/app\"
-mv ./docs/app/browser/* ./docs/app
-rimraf --glob ./docs/app/{browser,server}
+[ -d $BUILD_OUTPUT ] && rimraf $BUILD_OUTPUT
+
+pnpm build -- --baseHref=\"/$REPO_NAME/\" --outputPath=\"$BUILD_OUTPUT\"
+mv $BUILD_OUTPUT/browser/* ./docs/app
+rimraf --glob $BUILD_OUTPUT/{browser,server}
 cnp
 
 exit $?
