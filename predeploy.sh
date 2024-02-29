150e390d23368217e24b6eb07f2e9451e89dd49a
Messi Inter Miami <136736943+MessiInter@users.noreply.github.com>
2024-02-29 22:31:12 +0700

    Revert "Update .eslintrc.base.json and package.json"

--- predeploy.sh
+++ predeploy.sh
@@ -2,13 +2,10 @@
 set -e
 
 REPO_NAME=$(node -p "require(\"git-repo-name\").sync()")
-BUILD_OUTPUT=./docs/app
 
-[ -d $BUILD_OUTPUT ] && rimraf $BUILD_OUTPUT
-
-pnpm build -- --baseHref=\"/$REPO_NAME/\" --outputPath=\"$BUILD_OUTPUT\"
-mv $BUILD_OUTPUT/browser/* ./docs/app
-rimraf --glob $BUILD_OUTPUT/{browser,server}
+pnpm build -- --baseHref=\"/$REPO_NAME/\" --outputPath=\"./docs\"
+mv ./docs/browser/* ./docs
+rimraf --glob ./docs/{browser,server}
 cnp
 
 exit $?
